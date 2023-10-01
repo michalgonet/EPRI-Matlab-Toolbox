d2b8927197fdd28aaa1c2a1e08b565110fdd6ab8
Michał Gonet <77459217+michalgonet@users.noreply.github.com>
2023-10-01 20:17:53 +0200

    Update interp_example.m

--- interp_example.m
+++ interp_example.m
@@ -1,22 +1,18 @@
 clc;clear;close all
 
-% Zdefiniowanie oryginalnego widma
 pkt = 64;
 x = linspace(1,pkt,pkt);
 x0 = round(pkt/2);
 fwhm = 20;
 [L,l] = lorenzian_shape(x,x0,fwhm);
 
-% Liczba punktów 
 new_pkt = 32;
 
 old = linspace(1,pkt,pkt);
 new = linspace(1,pkt,new_pkt);
 
-% Interpolacja
 l_new = interp1(old,l,new);
 
-% Wizualizacja wyników
 plot(old,l,'ko-',new,0.01+l_new,'kd-');
 legend(['Oryginalne widmo; ',num2str(pkt),' pkt'],['Interpolowane widmo; ',num2str(new_pkt),' pkt'])
 
